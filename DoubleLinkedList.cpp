43a383f6d43ec65ea708f60eb3c446e48a723912
som <somsinghlodhi2323@gmail.com>
2022-07-05 09:43:04 +0530

    added function to calculate sum of all element

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -31,11 +31,21 @@ void display(Node *ptr ) {
         ptr=ptr->next ;
     }cout<<endl;
 }
+void sumOFAllElement(Node *ptr){
+    int sum = 0 ;
+    while(ptr!=nullptr){
+        sum+=ptr->data;
+        ptr=ptr->next;
+    }
+    cout<<"sum of element is "<<sum<<endl;
+}
+
 int main(){
    
     int a[4] = {1 , 10 , 100 , 1000} ;
     create(a , 4) ;
    // cout<<"hi";
     display(first) ;
+    sumOFAllElement(first);
  return 0 ;
 }
